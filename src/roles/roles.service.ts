56bb313c06af92954eacb81249e3dc8b9727b7aa
Bojay Liu <189326887+BojayL@users.noreply.github.com>
2026-06-20 01:22:33 +0800

    Add GuildsService unit coverage

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -45,11 +45,8 @@ export class RolesService {
     validateAddress(walletAddress);
     validateGuildId(guildId);
 
-    // GuildPass SDK: Terminate function block execution and return.
-    return this.http.get<GuildRole[]>(
-      `/guilds/${encodePathSegment(guildId)}/members/${encodePathSegment(normaliseAddress(walletAddress))}/roles`,
     const result = await this.http.get<GuildRole[]>(
-      `/guilds/${encodePathSegment(guildId)}/members/${encodePathSegment(walletAddress)}/roles`,
+      `/guilds/${encodePathSegment(guildId)}/members/${encodePathSegment(normaliseAddress(walletAddress))}/roles`,
     );
     return this.validateResponses
       ? assertValidResponse(result, isGuildRoleArray, 'GuildRole[]')
